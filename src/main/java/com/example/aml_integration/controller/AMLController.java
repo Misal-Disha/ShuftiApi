b45aadc313d1bbc5ce7d9d29c2e54337b8307dde
Disha Disha <dishamisal2614@gmail.com>
2024-10-17 14:16:01 +0530

    check

--- src/main/java/com/example/aml_integration/controller/AMLController.java
+++ src/main/java/com/example/aml_integration/controller/AMLController.java
@@ -28,10 +28,11 @@ public class AMLController {
         return ResponseEntity.ok("AML check processed successfully");
     }
     
-//    @PostMapping("/callback")
-//    public ResponseEntity<String> handleCallback(@RequestBody Map<String, Object> requestData) {
-//        System.out.println("Callback received: " + requestData);
-//
+    @PostMapping("/callback")
+    public ResponseEntity<String> handleCallback(@RequestBody String requestData) {
+    	System.out.println("Hiii");
+        System.out.println("Callback received: " + requestData);
+
 //        String reference = (String) requestData.get("reference");
 //        String event = (String) requestData.get("event");
 //        String verificationStatus = (String) requestData.get("verification_status");
@@ -39,42 +40,42 @@ public class AMLController {
 //        System.out.println("Reference: " + reference);
 //        System.out.println("Event: " + event);
 //        System.out.println("Verification Status: " + verificationStatus);
-//
-//        return ResponseEntity.ok("Callback response received!");
-//    }
-    
-    @PostMapping("/callback")
-    public ResponseEntity<String> handleCallback(@RequestBody Map<String, Object> requestData) {
-        System.out.println("Callback received: " + requestData);
 
-        String reference = (String) requestData.get("reference");
-        String event = (String) requestData.get("event");
-        String country = (String) requestData.get("country");
-
-        Map<String, Object> verificationData = (Map<String, Object>) requestData.get("verification_data");
-        Map<String, Object> backgroundChecks = (Map<String, Object>) verificationData.get("background_checks");
-        Map<String, Object> nameData = (Map<String, Object>) backgroundChecks.get("name");
-
-        String firstName = (String) nameData.get("first_name");
-        String lastName = (String) nameData.get("last_name");
-        String dob = (String) backgroundChecks.get("dob");
-
-        // Extract declined reason and codes
-        String declinedReason = (String) requestData.get("declined_reason");
-        List<String> declinedCodes = (List<String>) requestData.get("declined_codes");
-
-        // Print extracted data
-        System.out.println("Reference: " + reference);
-        System.out.println("Event: " + event);
-        System.out.println("Country: " + country);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("DOB: " + dob);
-        System.out.println("Declined Reason: " + declinedReason);
-        System.out.println("Declined Codes: " + declinedCodes);
-
-        return ResponseEntity.ok("Callback received and processed successfully");
+        return ResponseEntity.ok("Callback response received!");
     }
+    
+//    @PostMapping("/callback")
+//    public ResponseEntity<String> handleCallback(@RequestBody Map<String, Object> requestData) {
+//        System.out.println("Callback received: " + requestData);
+//
+//        String reference = (String) requestData.get("reference");
+//        String event = (String) requestData.get("event");
+//        String country = (String) requestData.get("country");
+//
+//        Map<String, Object> verificationData = (Map<String, Object>) requestData.get("verification_data");
+//        Map<String, Object> backgroundChecks = (Map<String, Object>) verificationData.get("background_checks");
+//        Map<String, Object> nameData = (Map<String, Object>) backgroundChecks.get("name");
+//
+//        String firstName = (String) nameData.get("first_name");
+//        String lastName = (String) nameData.get("last_name");
+//        String dob = (String) backgroundChecks.get("dob");
+//
+//        // Extract declined reason and codes
+//        String declinedReason = (String) requestData.get("declined_reason");
+//        List<String> declinedCodes = (List<String>) requestData.get("declined_codes");
+//
+//        // Print extracted data
+//        System.out.println("Reference: " + reference);
+//        System.out.println("Event: " + event);
+//        System.out.println("Country: " + country);
+//        System.out.println("First Name: " + firstName);
+//        System.out.println("Last Name: " + lastName);
+//        System.out.println("DOB: " + dob);
+//        System.out.println("Declined Reason: " + declinedReason);
+//        System.out.println("Declined Codes: " + declinedCodes);
+//
+//        return ResponseEntity.ok("Callback received and processed successfully");
+//    }
 
 
     @GetMapping("/redirect")
